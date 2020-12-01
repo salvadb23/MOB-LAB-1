9bc99ee3be13a4a3ea041c052652a6ecbd6f7993
Salvador Becerra <salvadorbecerra@Salvadors-MacBook-Air.local>
2020-12-01 02:29:38 -0800

    lab

---
+++ Lab1.playground/Pages/Types .xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+/*:
+ # Types and Type Safety
+ 
+ Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
+ */
+var firstDeciaml: Float = 0.1
+var secondDecimal: Float = 0.5
+
+
+/*:
+ Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var trueOrFalse: Bool = true
+print("not equal types")
+
+/*:
+ Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var str: String = "Test"
+print("not equal types")
+
+/*:
+ Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var wholeN: Int = 10
+print("not equal types")
+
+//: [Previous](@previous)  [Next](@next)
